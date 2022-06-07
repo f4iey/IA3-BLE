91fbc5ae7612ee87cfffd578221c7763efca2e02
AwAii19 <awaii19@gmail.com>
2022-06-07 13:38:27 +0200

    initial comit

---
+++ GNU ARM v10.2.1 - Default/gecko_sdk_4.0.2/platform/bootloader/app_properties/app_properties.d
@@ -0,0 +1,10 @@
+gecko_sdk_4.0.2/platform/bootloader/app_properties/app_properties.o: \
+ C\:/Users/Xen/SimplicityStudio/SDKs/gecko_sdk/platform/bootloader/app_properties/app_properties.c \
+ c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C\:\Users\Xen\SimplicityStudio\v5_workspace\soc_empty\config/app_properties_config.h \
+ C\:\Users\Xen\SimplicityStudio\v5_workspace\soc_empty\autogen/sl_application_type.h \
+ c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\bootloader\api\application_properties.h
+c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C\:\Users\Xen\SimplicityStudio\v5_workspace\soc_empty\config/app_properties_config.h:
+C\:\Users\Xen\SimplicityStudio\v5_workspace\soc_empty\autogen/sl_application_type.h:
+c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\bootloader\api\application_properties.h:
---
+++ GNU ARM v10.2.1 - Default/gecko_sdk_4.0.2/platform/common/toolchain/src/sl_memory.d
@@ -0,0 +1,10 @@
+gecko_sdk_4.0.2/platform/common/toolchain/src/sl_memory.o: \
+ C\:/Users/Xen/SimplicityStudio/SDKs/gecko_sdk/platform/common/toolchain/src/sl_memory.c \
+ c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_memory.h \
+ c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_memory_region.h \
+ C\:\Users\Xen\SimplicityStudio\v5_workspace\soc_empty\config/sl_memory_config.h
+c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_memory.h:
+c\:\users\xen\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_memory_region.h:
+C\:\Users\Xen\SimplicityStudio\v5_workspace\soc_empty\config/sl_memory_config.h:
